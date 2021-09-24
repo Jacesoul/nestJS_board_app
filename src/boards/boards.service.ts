98fcd8809cae892a03c8c3aa10b3159d9bcd13b7
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-09-25 00:14:37 +0900

    CREATE board model

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = [];
+  private boards: Board[] = []; // boards가 배열이기 때문에 Board도 []를 붙여서 배열로 만들어준다.
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
+    // return 값의 타입 지정
     return this.boards;
   }
 }
