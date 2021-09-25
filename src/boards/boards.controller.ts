7d322381b80e78ac6b519451b1aad7b265e94048
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-09-25 19:53:32 +0900

    CREATE createboard controller

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 
@@ -9,5 +9,15 @@ export class BoardsController {
   @Get('/')
   getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    console.log('title', title);
+    console.log('description', description);
+    return this.boardsService.createBoard(title, description);
   }
 }
