d1906c2ccba67c161f6f208a3d63aec7676cb786
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-10-03 21:30:42 +0900

    CREATE JWT Token logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,24 +4,32 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { User } from 'src/entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // 유저 토큰 생성 ( Secret + Payload )
+      const payload = { username }; // 토큰을 이용해 중요한 정보를 가져가기 쉽기 때문에 payload에는 중요한 정보를 넣으면 안된다.
+      const accessToken = await this.jwtService.sign(payload); //sign메소드가 Secret과 Payload를 합쳐서 accessToken을 만들어준다.
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login failed');
     }
