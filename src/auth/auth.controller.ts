d7a253c7ce7a6b9fda427e6cea554c705361c3f9
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-10-05 16:37:17 +0900

    Create custom decorator for getting User information directly

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import {
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from 'src/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -30,7 +32,7 @@ export class AuthController {
 
   @Post('/authTest')
   @UseGuards(AuthGuard()) // AuthGuard는 @nestjs/passport에서 가져온 기능이다.
-  authTest(@Req() req) {
-    console.log('req', req);
+  authTest(@GetUser() user: User) {
+    console.log('user', user);
   }
 }
